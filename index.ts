0ea412e04ca4c5c7ff898c4200491813f2743f57
Oleksandr Karpov <endway08@gmail.com>
2017-06-01 18:42:14 +0300

    rewrited on TypeScript and upgraded Output

---
+++ index.ts
@@ -0,0 +1,97 @@
+interface IComponentClass extends Function {
+    $inject?: Array<string>;
+    bindings?: {[key: string]: string};
+    controller: Object;
+    template: string;
+
+    constructor: IComponentClass;
+}
+
+export function Input(alias: string): PropertyDecorator {
+    return function (target: IComponentClass, property: string) {
+        if (!target.constructor.bindings) {
+            target.constructor.bindings = {};
+        }
+
+        target.constructor.bindings[property] = "<" + (alias ? alias : "");
+    };
+}
+
+export function Output(alias: string): PropertyDecorator {
+    return function (target: IComponentClass, property: string) {
+        if (!target.constructor.bindings) {
+            target.constructor.bindings = {};
+        }
+
+        const privateCallbackName = `__${property}`;
+        const attrBinding = alias ? alias : property;
+        let callbackCache: Function;
+        let eventEmitterCache: {subscribe: Function};
+
+        target.constructor.bindings[privateCallbackName] = `&${attrBinding}`;
+
+        Object.defineProperty(target, privateCallbackName, {
+            set: function (callback: Function) {
+                if (typeof callback === "function") {
+                    callbackCache = callback;
+                }
+            },
+            enumerable: false,
+            configurable: true
+        });
+
+        Object.defineProperty(target, property, {
+            set: function (eventEmitterInstance) {
+                if (eventEmitterInstance && eventEmitterInstance.subscribe) {
+                    eventEmitterInstance.subscribe(function (eventData) {
+                        callbackCache({$event: eventData});
+                    });
+                    eventEmitterCache = eventEmitterInstance;
+                }
+            },
+            get: function () {
+                return eventEmitterCache;
+            },
+            enumerable: true,
+            configurable: true
+        });
+    };
+}
+
+export function Inject(dependencyName: string): ParameterDecorator {
+    return function (target: IComponentClass, property: string, parameterIndex: number) {
+        if (!target.$inject) {
+            target.$inject = [];
+        }
+
+        target.$inject[parameterIndex] = dependencyName;
+    };
+}
+
+export function Component(config: {template?: string}): ClassDecorator {
+    return function (target: IComponentClass) {
+        if (config) {
+            if (config.template) {
+                target.template = config.template;
+            }
+        }
+
+        // Lifecycle hooks aliases
+        if (target.prototype.ngOnInit) {
+            target.prototype.$onInit = target.prototype.ngOnInit;
+        }
+
+        if (target.prototype.ngOnChanges) {
+            target.prototype.$onChanges = target.prototype.ngOnChanges;
+        }
+
+        if (target.prototype.ngOnDestroy) {
+            target.prototype.$onDestroy = target.prototype.ngOnDestroy;
+        }
+
+        // Controller linking
+        target.controller = target;
+
+        return target;
+    }
+}
