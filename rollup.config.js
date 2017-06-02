42275f76972fe20a6c4b8210d4e9d0147cfeeb50
Alexandr Rodik <rodik@readdle.com>
2017-06-02 17:09:50 +0300

    fix: building into dist directory, add ignore locations

--- rollup.config.js
+++ rollup.config.js
@@ -4,16 +4,16 @@ import rollupAlias from "rollup-plugin-alias";
 import typescript from "rollup-plugin-typescript";
 
 export default {
-	entry: "example/src/app.ts",
-	format: "iife",
-	dest: "example/bundle.js",
-	plugins: [
-		rollupAlias({
-			tslib: "node_modules/tslib/tslib.es6.js",
-			// angular: "node_modules/angular/angular.js"
+    entry: "example/src/app.ts",
+    format: "iife",
+    dest: "example/bundle.js",
+    plugins: [
+        rollupAlias({
+            tslib: "node_modules/tslib/tslib.es6.js",
+            // angular: "node_modules/angular/angular.js"
         }),
-		typescript({
-			typescript: ts
-		})
-	]
+        typescript({
+            typescript: ts
+        })
+    ]
 }
