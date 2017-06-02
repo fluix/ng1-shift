bd4b25bba1b2794fec8d06c17b7794540d2e9729
Oleksandr Karpov <endway08@gmail.com>
2017-06-02 13:04:00 +0300

    fix types

--- test/inject.test.ts
+++ test/inject.test.ts
@@ -1,14 +1,17 @@
 import {Inject} from "../index";
 
 class Test {
-    constructor(@Inject("service") service: Function) {};
+    constructor(
+        @Inject("service") service: Function,
+        @Inject("service2") service2: Function
+    ) {};
 }
 
 describe("Inject decorator", function() {
     let instance;
 
     beforeEach(function() {
-        instance = new Test(() => {});
+        instance = new Test(() => {}, () => {});
     });
 
     test("should create $inject", function () {
@@ -17,5 +20,13 @@ describe("Inject decorator", function() {
 
     test("should add `service` to $inject", function () {
         expect(instance.constructor.$inject).toContain("service");
+    });
+
+    test("should add `service` to the first place in $inject", function () {
+        expect(instance.constructor.$inject[0]).toEqual("service");
+    });
+
+    test("should add `service2` to the second place in $inject", function () {
+        expect(instance.constructor.$inject[1]).toEqual("service2");
     });
 });
--- test/output.test.ts
+++ test/output.test.ts
@@ -1,4 +1,4 @@
-import {Output} from "../index";
+import {EventEmitter, Output} from "../index";
 
 class Test {
     @Output("onAliasCallback") onCallback: Function;
@@ -15,15 +15,30 @@ describe("Output decorator", function() {
         expect(instance.constructor.bindings).toBeDefined();
     });
 
-    test("should add `onCallback` to bindings", function () {
-        expect(instance.constructor.bindings.onCallback).toBeDefined();
+    test("should add `onCallback` to bindings with prefix `__`", function () {
+        expect(instance.constructor.bindings.__onCallback).toBeDefined();
     });
 
     test("should add `onCallback` to bindings as callback", function () {
-        expect(instance.constructor.bindings.onCallback[0]).toEqual("&");
+        expect(instance.constructor.bindings.__onCallback[0]).toEqual("&");
     });
 
     test("should add `onCallback` to bindings with alias to `onAliasCallback`", function () {
-        expect(instance.constructor.bindings.onCallback).toEqual("&onAliasCallback");
+        expect(instance.constructor.bindings.__onCallback).toEqual("&onAliasCallback");
+    });
+
+    test("should subscribe to EventEmitter `__onCallback`", function () {
+        instance.onCallback = new EventEmitter();
+        expect(instance.onCallback.listeners.length).toEqual(1);
+    });
+
+    test("should call callback when emit EventEmitter", function () {
+        const callback = jest.fn();
+
+        instance.__onCallback = callback;
+        instance.onCallback = new EventEmitter();
+        instance.onCallback.emit();
+
+        expect(callback).toHaveBeenCalled();
     });
 });
