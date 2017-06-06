ae360c05b8a9dd1ffb4de360fe9173a78d7c3f0f
Alexandr Rodik <rodik@readdle.com>
2017-06-06 16:46:39 +0300

    fix: bundle the UMD module

--- rollup.config.dist.js
+++ rollup.config.dist.js
@@ -1,17 +1,13 @@
 import * as path from "path";
 import ts from "typescript";
-import rollupAlias from "rollup-plugin-alias";
 import typescript from "rollup-plugin-typescript";
 
 export default {
     entry: "./index.ts",
     dest: "./dist/index.js",
-    format: "iife",
-    moduleName: "ng1shift",
+    format: "amd",
+    moduleName: "ng1-shift",
     plugins: [
-        rollupAlias({
-            tslib: "node_modules/tslib/tslib.es6.js",
-        }),
         typescript({
             typescript: ts
         })
