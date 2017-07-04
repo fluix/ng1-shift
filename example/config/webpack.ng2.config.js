81230a9f21914abbd0a79181a012780968bfb33c
Anton Derenivsky <anton.derenivsky@gmail.com>
2017-07-04 16:22:41 +0300

    Fixed ng2 check

--- example/config/webpack.ng2.config.js
+++ example/config/webpack.ng2.config.js
@@ -1,4 +1,5 @@
 const assign = require("object-assign");
+const DefinePlugin = require("webpack/lib/DefinePlugin");
 const ContextReplacementPlugin = require("webpack/lib/ContextReplacementPlugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const baseConfig = require("./webpack.base.config");
@@ -15,6 +16,11 @@ const plugins = [
         template: "./index.ejs",
         inject: "body",
         NG2: true
+    }),
+    new DefinePlugin({
+        process: {
+            env: { NG2: true }
+        }
     })
 ];
 
