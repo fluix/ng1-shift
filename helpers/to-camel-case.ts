b8b7d2c4d31963cc92e5d0c362fe3640a12a4953
Anton Derenivsky <anton.derenivsky@gmail.com>
2017-07-12 15:31:04 +0300

    Fixed component name registration

--- helpers/to-camel-case.ts
+++ helpers/to-camel-case.ts
@@ -1,14 +1,13 @@
 export default function toCamelCase(str: string) {
-    let newStr = "";
-    let newArr = [];
+    if (str.indexOf("-") !== -1) {
+        const newArr = str.split("-");
 
-    if (str.indexOf("-") != -1) {
-        newArr = str.split("-");
         for (var i = 1 ; i < newArr.length ; i++) {
             newArr[i] = newArr[i].charAt(0).toUpperCase() + newArr[i].substr(1);
         }
-        newStr = newArr.join("");
+
+        return newArr.join("");
     }
 
-    return newStr;
+    return str;
 }
