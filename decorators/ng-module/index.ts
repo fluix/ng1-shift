26ffbca14d3b56a992757c0e3fc9757fd4040639
Anton Derenivsky <anton.derenivsky@gmail.com>
2017-07-13 11:02:09 +0300

    Added direct register

--- decorators/ng-module/index.ts
+++ decorators/ng-module/index.ts
@@ -2,7 +2,7 @@ import importHandler from "./metadata-handlers/import";
 import daclarationHandler from "./metadata-handlers/declaration";
 import providerHandler from "./metadata-handlers/provider";
 
-export function NgModule({imports, declarations, providers, ng1RunConfig}: any) {
+export function NgModule({imports, declarations, providers, directRegister}: any) {
     return function (target: any) {
         let ng1ModuleIds: Array<string> = [];
         let ng1Module = angular.module(target.name, ng1ModuleIds);
@@ -17,6 +17,10 @@ export function NgModule({imports, declarations, providers, ng1RunConfig}: any) 
 
         if (providers && providers.length) {
             providerHandler(ng1Module, providers, declarations);
+        }
+
+        if (directRegister) {
+            directRegister(ng1Module);
         }
 
         target.ng1Module = ng1Module;
