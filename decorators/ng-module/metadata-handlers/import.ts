875695ef947d045a58f2311c63a5070c992dbb16
Anton Derenivsky <anton.derenivsky@gmail.com>
2017-07-19 15:16:08 +0300

    Added types

--- decorators/ng-module/metadata-handlers/import.ts
+++ decorators/ng-module/metadata-handlers/import.ts
@@ -1,12 +1,14 @@
-export default function importHandler(imports: any) {
+type ng1Module = { $inject?: Array<string> } | any;
+
+export default function importHandler(imports: Array<ng1Module>) {
     let ng1ModuleIds: Array<string> = [];
 
-    const modules = imports.filter( ({ $inject }: any) => {
-        return !($inject && $inject.find((i: any) => i === "$stateProvider"))
+    const modules = imports.filter((mdl: ng1Module) => {
+        return !(mdl.$inject && mdl.$inject.find((i: any) => i === "$stateProvider"));
     });
 
-    const ng1RouterConfig = imports.find(({ $inject }: any) => {
-        return $inject && $inject.find((i: any) => i === "$stateProvider")
+    const ng1RouterConfig = imports.find((mdl: ng1Module) => {
+        return mdl.$inject && mdl.$inject.find((i: any) => i === "$stateProvider");
     });
 
     if (modules.length) {
