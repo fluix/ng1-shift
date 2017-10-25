8cbc48c1c3373fa605655e1200632a5831fa5434
Oleksandr Karpov <endway08@gmail.com>
2017-10-25 16:24:56 +0300

    added ngAfterViewInit tests

--- test/component.test.ts
+++ test/component.test.ts
@@ -5,6 +5,7 @@ import {Component} from "../index";
 })
 class Test {
     ngOnInit() {}
+    ngAfterViewInit() {}
     ngOnChanges() {}
     ngOnDestroy() {}
 }
@@ -27,6 +28,11 @@ describe("Component decorator", function() {
     test("should link `ngOnInit` to `$onInit`", function () {
         expect(instance.$onInit).toBeDefined();
         expect(instance.$onInit).toEqual(instance.ngOnInit);
+    });
+
+    test("should link `ngAfterViewInit` to `$postLink`", function () {
+        expect(instance.$postLink).toBeDefined();
+        expect(instance.$postLink).toEqual(instance.ngAfterViewInit);
     });
 
     test("should link `ngOnChanges` to `$onChanges`", function () {
