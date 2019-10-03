9e70dbda596b933a7327354cb97872c2b471ff91
danilradkovsky <dradkovsky@readdle.com>
2019-10-03 15:30:19 +0300

    Fix argument type for emit

--- decorators/event_emitter.ts
+++ decorators/event_emitter.ts
@@ -1,7 +1,7 @@
 export class EventEmitter<T extends any = any> {
     private listeners: Array<(event: T) => void> = [];
 
-    emit(event: T | null = null) {
+    emit(event?: T) {
         this.listeners.forEach(callback => callback.call(null, event));
     }
 
